33b979ff8d5f2f31e4c6646fa023d05ee4a169cb
Akbar Fachriansyah <akbar.fachriansyah@gmail.com>
2019-11-15 14:04:11 +0700

    init repo commit

---
+++ index.php
@@ -0,0 +1,280 @@
+
+<?php 
+	include_once 'layout/header.php';
+?>
+
+<section id="home" class="banner">
+	<div class="bg-banner" style="background-image: url('images/neonbrand-381344-unsplash.jpg');">
+		<div class="container">
+			<div class="col-xs-12 col-sm-12 col-md-6 px-0">
+				<div class="text-banner">
+					<h1>Feel at home <br /> in a beautiful room </h1>
+					<div class="desc-banner">
+						<p>Enjoy Casual Mediterranean dishes with combination of Italian and Spanish cuisine you’ve never experienced before at Sea Grain Restaurant & Bar. </p>
+					</div>
+					<div class="subscribe-banner">
+						<div class="input-banner input-group mb-3">
+						  <input type="text" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="button-addon2">
+						  <div class="input-group-append">
+						    <button class="btn btn-primary" type="button" id="button-addon2">SUBSCRIBE NOW</button>
+						  </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="highlight" class="highlight mt-5em">
+	<div class="highlight-wrapper">
+		<div class="container">
+			<div class="highlight-title text-center">
+				<h2 class="font-weight-bold">Highlights</h2>
+			</div>
+			<div class="highlight-content">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-11 mx-auto">
+						<div class="row">
+							<div class="col-xs-12 col-sm-12 col-md-6">
+								<div class="media">
+								  	<div class="highlight-icon">
+								  		<div class="icon-box">
+								  			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7 13c1.66 0 3-1.34 3-3S8.66 7 7 7s-3 1.34-3 3 1.34 3 3 3zm12-6h-8v7H3V5H1v15h2v-3h18v3h2v-9c0-2.21-1.79-4-4-4z"/></svg>
+								  		</div>
+								  	</div>
+								  	<div class="media-body">
+								    	<h5 class="mt-0 mb-3">250 guest room</h5>
+								    	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices dolor ac risus ornare, ac molestie enim eleifend. Fusce at dapibus sapien.
+								  	</div>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-12 col-md-6">
+								<div class="media">
+								  	<div class="highlight-icon">
+								  		<div class="icon-box">
+								  			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 9H9V2H7v7H5V2H3v7c0 2.12 1.66 3.84 3.75 3.97V22h2.5v-9.03C11.34 12.84 13 11.12 13 9V2h-2v7zm5-3v8h2.5v8H21V2c-2.76 0-5 2.24-5 4z"/><path fill="none" d="M0 0h24v24H0z"/></svg>
+								  		</div>
+								  	</div>
+								  	<div class="media-body">
+								    	<h5 class="mt-0 mb-3">Tempting Restaurant</h5>
+								    	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices dolor ac risus ornare, ac molestie enim eleifend. Fusce at dapibus sapien.
+								  	</div>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-12 col-md-6">
+								<div class="media">
+								  	<div class="highlight-icon">
+								  		<div class="icon-box">
+								  			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0v24h24V0H0zm23 16c0 1.1-.9 2-2 2H3c-1.1 0-2-.9-2-2V8c0-1.1.9-2 2-2h18c1.1 0 2 .9 2 2v8z"/><path d="M21 6H3c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-10 7H8v3H6v-3H3v-2h3V8h2v3h3v2zm4.5 2c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4-3c-.83 0-1.5-.67-1.5-1.5S18.67 9 19.5 9s1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/></svg>
+								  		</div>
+								  	</div>
+								  	<div class="media-body">
+								    	<h5 class="mt-0 mb-3">Kid's club</h5>
+								    	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices dolor ac risus ornare, ac molestie enim eleifend. Fusce at dapibus sapien.
+								  	</div>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-12 col-md-6">
+								<div class="media">
+								  	<div class="highlight-icon">
+								  		<div class="icon-box">
+								  			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zm-6 0h-4V4h4v2z"/></svg>
+								  		</div>
+								  	</div>
+								  	<div class="media-body">
+								    	<h5 class="mt-0 mb-3">Space meeting room</h5>
+								    	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices dolor ac risus ornare, ac molestie enim eleifend. Fusce at dapibus sapien.
+								  	</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="gallery" class="gallery">
+	<div class="gallery-landing">
+		<div id="owl-gallery" class="owl-carousel owl-theme">
+			<div class="gallery-item">
+				<img src="images/naomi-hebert-188183-unsplash.png" />
+			</div>
+			<div class="gallery-item">
+				<img src="images/sidekix-media-1129788-unsplash.png" />
+			</div>
+			<div class="gallery-item">
+				<img src="images/nathan-fertig-249917-unsplash.png" />
+			</div>
+		</div>	
+	</div>
+</section>
+<section id="testimonial" class="testimonial mt-5em">
+	<div class="testimonial-wrapper">
+		<div class="container">
+			<div class="testimonial-title">
+				<h2 class="font-weight-bold">Testimonial</h2>
+			</div>
+			<div class="testimonial-content">
+				<div id="owl-testi" class="owl-carousel owl-theme">
+				  	<div class="testimonial-item">
+				  		<div class="media">
+						  <img src="images/avatar1.jpg" class="mr-3" alt="...">
+						  <div class="media-body">
+						    <h5 class="mt-0 font-weight-bold">Victoria Lee</h5>
+						    	Tellus quisque
+						  </div>
+						</div>
+						<blockquote>
+							<p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+						</blockquote>	
+				  	</div>
+				  	<div class="testimonial-item">
+				  		<div class="media">
+						  <img src="images/avatar2.jpg" class="mr-3" alt="...">
+						  <div class="media-body">
+						    <h5 class="mt-0 font-weight-bold">Victoria Lee</h5>
+						    	Tellus quisque
+						  </div>
+						</div>
+						<blockquote>
+							<p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+						</blockquote>	
+				  	</div>
+				  	<div class="testimonial-item">
+				  		<div class="media">
+						  <img src="images/avatar3.jpg" class="mr-3" alt="...">
+						  <div class="media-body">
+						    <h5 class="mt-0 font-weight-bold">Victoria Lee</h5>
+						    	Tellus quisque
+						  </div>
+						</div>
+						<blockquote>
+							<p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+						</blockquote>	
+				  	</div>
+				  	<div class="testimonial-item">
+				  		<div class="media">
+						  <img src="images/avatar3.jpg" class="mr-3" alt="...">
+						  <div class="media-body">
+						    <h5 class="mt-0 font-weight-bold">Victoria Lee</h5>
+						    	Tellus quisque
+						  </div>
+						</div>
+						<blockquote>
+							<p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+						</blockquote>	
+				  	</div>
+				  	<div class="testimonial-item">
+				  		<div class="media">
+						  <img src="images/avatar3.jpg" class="mr-3" alt="...">
+						  <div class="media-body">
+						    <h5 class="mt-0 font-weight-bold">Victoria Lee</h5>
+						    	Tellus quisque
+						  </div>
+						</div>
+						<blockquote>
+							<p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+						</blockquote>	
+				  	</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="promo mt-5em">
+	<div class="bg-promo">
+		<img src="images/sven-brandsma-1379481-unsplash.png" class="img-fluid" />
+		<div class="content-promo">
+			
+			<h2 class="mb-3 font-weight-bold">Get promo now</h2>
+			<p>Aenean sed lorem est. Sed quis neque ut nibh suscipit imperdiet ac non augue. Aenean ornare sit amet lectus non tristique. Nunc ut volutpat lectus. Nulla velit 		augue, pulvinar sed nisi sit amet, eleifend fermentum est.</p>
+			<div class="input-banner input-group mt-5 mb-3">
+			  <input type="text" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="button-addon2">
+			  <div class="input-group-append">
+			    <button class="btn btn-primary" type="button" id="button-addon2">SUBSCRIBE NOW</button>
+			  </div>
+			</div>
+			
+		</div>
+		<div class="overlay"></div>
+	</div>
+</section>
+
+<?php 
+	include_once 'layout/footer.php';
+?>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+
+		$(".navi-link a").on('click', function(event) {
+
+		    if (this.hash !== "") {
+
+		      var hash = this.hash;
+
+		      $('html, body').animate({
+		        scrollTop: $(hash).offset().top
+		      }, 1000, function() {
+
+		        window.location.hash = hash;
+		      });
+		      return false;
+		    } 
+
+	  });
+
+	  	var owl = $("#owl-testi").owlCarousel({
+	  		dots: true,
+	  		stagePadding: 50,
+	  		items: 2,
+	  		responsive : {
+	  			480 : {
+			        items : 1
+			    },
+	  			640 : {
+			        items : 1
+			    },
+			    768 : {
+			        items : 2
+			    }
+			}
+	  	});
+
+	  	checkClasses();
+	    owl.on('translated.owl.carousel', function(event) {
+	        checkClasses();
+	    });
+
+	    $("#owl-gallery").owlCarousel({
+	  		items: 3,
+	  		autoWidth:true
+	  	});
+
+	});
+
+	$(window).scroll(function(){
+	    if ($(window).scrollTop() >= 300) {
+	        $('header').addClass('fixed');
+	        $('body').addClass('scrolled');
+	    }
+	    else {
+	        $('header').removeClass('fixed');
+	        $('body').removeClass('scrolled');
+	    }
+	});
+
+    function checkClasses(){
+        var total = $('#owl-testi .owl-stage .owl-item.active').length;
+        
+        $('#owl-testi .owl-stage .owl-item').removeClass('itemActive');
+        
+        $('#owl-testi .owl-stage .owl-item.active').each(function(index){
+            if (index === 0) {
+                // this is the first one
+                $(this).addClass('itemActive');
+            }
+        });
+    }
+</script>
---
+++ layout/header.php
@@ -0,0 +1,35 @@
+  <!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+      <meta name="generator" content="Jekyll v3.8.5">
+      <title>Landing - Hotel</title>
+
+      <!-- Bootstrap core CSS -->
+      <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link href="plugins/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+      <link href="plugins/owl-carousel/owl.theme.default.min.css" rel="stylesheet">
+      <!-- Custom styles for this template -->
+      <link href="css/main.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+      <header>
+        <div class="container">
+          <div class="d-flex flex-column flex-md-row align-items-center">
+            <a href="./" class="my-0 mr-md-auto">
+              <h5 class="font-weight-bold">Landing</h5>
+            </a>
+            <nav class="navi-link my-2 my-md-0 mr-md-3">
+              <a class="text-dark" href="#home">Home</a>
+              <a class="text-dark" href="#highlight">Highlights</a>
+              <a class="text-dark" href="#testimonial">Testimonial</a>
+            </nav>
+            <a class="btn btn-outline-primary" href="#">SUBSCRIBE NOW</a>
+          </div>
+        </div>  
+      </header>  
